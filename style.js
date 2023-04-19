38ad9127a65650d92a2830a26c5e96b7e17b0eae
fe2oz <93234748+fe2oz@users.noreply.github.com>
2023-04-19 20:55:03 +0900

    2023.04.19_22:55

---
+++ style.js
@@ -0,0 +1,7 @@
+var sc01_p = document.querySelectorAll('.bot-right p');
+var lineDashed = document.querySelectorAll('.bot-right .line_dash'); 
+
+for(var i=0; i<sc01_p.length; i++){
+    var sc01_p_width = sc01_p[i].clientWidth;
+    lineDashed[i].style.width = "calc("+100%+" - ${sc01_p_width}px)";
+}
