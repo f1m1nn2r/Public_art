fd339e5af425c97368d47e594e806c1b85ce56e0
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-04-19 23:56:48 +0900

    2023.04.19_23:56

--- style.js
+++ style.js
@@ -3,5 +3,5 @@ var lineDashed = document.querySelectorAll('.bot-right .line_dash');
 
 for(var i=0; i<sc01_p.length; i++){
     var sc01_p_width = sc01_p[i].clientWidth;
-    lineDashed[i].style.width = "calc("+100%+" - ${sc01_p_width}px)";
+    lineDashed[i].style.width = `calc(100% - ${sc01_p_width}px)`;
 }
